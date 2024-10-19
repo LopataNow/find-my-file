7382b849108eb9e72377e984da1462341de986ac
Peter Kopáč <lopata.kopac@gmail.com>
2024-10-19 13:52:17 +0200

    Update userFetch data hook for global data

--- frontend/src/hooks/use-fetch-data.ts
+++ frontend/src/hooks/use-fetch-data.ts
@@ -9,6 +9,7 @@ export interface FetchData<T> {
 }
 
 const dataMap = new Map<string, FetchData<any>>();
+const subscriptions: any[] = [];
 
 function setDataMap<T>(dependecies: any[], data: FetchData<T>) {
     dataMap.set(dependecies.map((d) => d.toString()).join('-'), data);
@@ -18,23 +19,43 @@ function getDataMap<T>(dependecies: any[]): FetchData<T> {
     return dataMap.get(dependecies.map((d) => d.toString()).join('-')) || { data: undefined, loading: false };
 }
 
+function updateSupcruptions() {
+    for (const subscription of subscriptions) {
+        subscription({});
+    }
+}
+
 function useFetchData<T>(dependecies: any[], fetch: ()=>Promise<T>): FetchData<T> {
     const [, setData] = useState({});
 
     const fetchNemo = useCallback(fetch, dependecies);
 
     useEffect(() => {
+        subscriptions.push(setData);
+        return () => {
+            const index = subscriptions.indexOf(setData);
+            if (index !== -1) {
+                subscriptions.splice(index, 1);
+            }
+        };
+    }, []);
+
+    useEffect(() => {
         const fetchData = async () => {
+        if (getDataMap(dependecies)?.data) {
+            return;
+        }
+
         try {
-            setData({});
+            updateSupcruptions();
             setDataMap(dependecies, { data: undefined, loading: true, error: '' });
 
             const result = await fetchNemo();
-            setData({});
+            updateSupcruptions();
 
             setDataMap(dependecies, { data: result, loading: false, error: '' });
         } catch (error) {
-            setData({ });
+            updateSupcruptions();
             setDataMap(dependecies, { data: undefined, loading: false, error: error });
         }
     };
