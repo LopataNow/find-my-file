eb2075f0f7ac121a22ac8b8258514831c0ba9028
Peter Kopáč <lopata.kopac@gmail.com>
2024-10-19 13:33:31 +0200

    Revert "Update userFetch data hook for global data"
    This reverts commit 63373a5482835d5aaa9ae31b2618f9a813a3ea31.

--- frontend/src/hooks/use-fetch-data.ts
+++ frontend/src/hooks/use-fetch-data.ts
@@ -8,41 +8,30 @@ export interface FetchData<T> {
     error?: any;
 }
 
-const dataMap = new Map<string, FetchData<any>>();
-
-function setDataMap<T>(dependecies: any[], data: FetchData<T>) {
-    dataMap.set(dependecies.map((d) => d.toString()).join('-'), data);
-}
-
-function getDataMap<T>(dependecies: any[]): FetchData<T> {
-    return dataMap.get(dependecies.map((d) => d.toString()).join('-')) || { data: undefined, loading: false };
-}
-
-function useFetchData<T>(dependecies: any[], fetch: ()=>Promise<T>): FetchData<T> {
-    const [, setData] = useState({});
+function useFetchData<T>(dependecies:any[], fetch: ()=>Promise<T>): FetchData<T> {
+    const [data, setData] = useState<FetchData<T>>({ data: undefined, loading: true, error: '' });
 
     const fetchNemo = useCallback(fetch, dependecies);
 
     useEffect(() => {
         const fetchData = async () => {
         try {
-            setData({});
-            setDataMap(dependecies, { data: undefined, loading: true, error: '' });
-
+            setData({ data: undefined, loading: true, error: '' });
             const result = await fetchNemo();
-            setData({});
-
-            setDataMap(dependecies, { data: result, loading: false, error: '' });
+            setData({
+                data: result,
+                loading: false,
+                error: ''
+            });
         } catch (error) {
-            setData({ });
-            setDataMap(dependecies, { data: undefined, loading: false, error: error });
+            setData({ data: undefined, loading: false, error: error });
         }
     };
 
         fetchData();
     }, [fetchNemo]);
 
-    return getDataMap(dependecies);
+    return data;
 };
 
 export default useFetchData;
